c3023d39cf2de468bce5f3d83e3826ed4ff00198
mdcruz <marie.cruz1@news.co.uk>
2020-07-19 16:28:04 +0100

    slight refactoring of the test

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -1,21 +1,20 @@
 /// <reference types="cypress"/>
 
 describe('Todo application', () => {
-  it('should log any accessibility failures', () => {
+  beforeEach(() => {
     cy.visit('http://todomvc.com/examples/react');
     cy.injectAxe();
+  });
+
+  it('should log any accessibility failures', () => {
     cy.checkA11y();
   });
 
   it('should exclude specific elements on the page', () => {
-    cy.visit('http://todomvc.com/examples/react');
-    cy.injectAxe();
     cy.checkA11y({ exclude: ['.learn'] });
   });
 
   it('should only test specific element on the page', () => {
-    cy.visit('http://todomvc.com/examples/react');
-    cy.injectAxe();
     cy.checkA11y('.learn');
   });
 });
