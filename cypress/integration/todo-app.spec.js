e416563607fb277cc04c5e7a5a88d30124d2fbb5
mdcruz <marie.cruz1@news.co.uk>
2020-07-19 16:27:01 +0100

    added two tests for testing specific elements in cypress-axe

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -6,4 +6,16 @@ describe('Todo application', () => {
     cy.injectAxe();
     cy.checkA11y();
   });
+
+  it('should exclude specific elements on the page', () => {
+    cy.visit('http://todomvc.com/examples/react');
+    cy.injectAxe();
+    cy.checkA11y({ exclude: ['.learn'] });
+  });
+
+  it('should only test specific element on the page', () => {
+    cy.visit('http://todomvc.com/examples/react');
+    cy.injectAxe();
+    cy.checkA11y('.learn');
+  });
 });
