eb013a7cf3fe0d80984e3cb3ecfa0618409a91b1
mdcruz <marie.cruz1@news.co.uk>
2020-07-22 22:21:38 +0100

    added two tests to demo excluding accessibility rules

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -17,4 +17,16 @@ describe('Todo application', () => {
   it('should only test specific element on the page', () => {
     cy.checkA11y('.learn');
   });
+
+  it('should only include rules with serious and critical impacts', () => {
+    cy.checkA11y(null, { includedImpacts: ['critical', 'serious'] });
+  });
+
+  it('should exclude specific accessibility rules', () => {
+    cy.checkA11y(null, {
+      rules: {
+        'color-contrast': { enabled: false },
+      },
+    });
+  });
 });
